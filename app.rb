ca6876f357d90dcb5bade77462c032c2244dab3e
Thomas Harker <thomasharker@Thomass-MacBook-Pro.local>
2016-06-15 13:28:37 +0100

    Add development database

--- app.rb
+++ app.rb
@@ -1,10 +1,12 @@
+ENV["RACK_ENV"] ||= "development"
+
 require 'sinatra/base'
 require './models/link'
 
 class Bookmarks < Sinatra::Base
 
   get '/' do
-    'HELLO YOU'
+    redirect '/links'
   end
 
   get '/links' do
