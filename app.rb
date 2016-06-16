36367a676514f79221ae4cb59b453236b6bca8c0
Thomas Harker <tom.harker@hotmail.co.uk>
2016-06-16 13:39:49 +0100

    Add password and user functionality

--- app.rb
+++ app.rb
@@ -6,6 +6,9 @@ require_relative 'data_mapper_setup'
 
 class Bookmarks < Sinatra::Base
 
+	enable :sessions
+	set :session_secret, 'super secret'
+
   get '/' do
     redirect '/links'
   end
@@ -36,6 +39,22 @@ class Bookmarks < Sinatra::Base
     erb :'links/index'
   end
 
+  get '/users/new' do
+  	erb :'users/new'
+  end
+
+  post '/users' do
+  	user = User.create(email: params[:email],
+  										password: params[:password])
+  	session[:user_id] = user.id
+  	redirect to('/links')
+  end
+
+  helpers do
+  	def current_user
+  		@current_user ||= User.get(session[:user_id])
+  	end
+  end
 
   run! if app_file == $0
 end
---
+++ models/user.rb
@@ -0,0 +1,16 @@
+require 'data_mapper'
+require 'dm-postgres-adapter'
+require 'BCrypt'
+
+class User
+	include DataMapper::Resource
+
+	property :id, Serial
+	property :email, String
+	property :password_digest, String, length: 60
+
+	def password=(password)
+		self.password_digest = BCrypt::Password.create(password)
+	end
+
+end
