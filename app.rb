3a543fea9c0b62f355f2dd7bd96bb62e90e0f92e
Thomas Harker <tom.harker@hotmail.co.uk>
2016-06-17 09:01:23 +0100

    Add sign in process

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 ENV["RACK_ENV"] ||= "development"
 
 require 'sinatra/base'
+require 'sinatra/flash'
 require './models/link'
 require_relative 'data_mapper_setup'
 
@@ -8,6 +9,7 @@ class Bookmarks < Sinatra::Base
 
 	enable :sessions
 	set :session_secret, 'super secret'
+	register Sinatra::Flash
 
   get '/' do
     redirect '/links'
@@ -40,15 +42,38 @@ class Bookmarks < Sinatra::Base
   end
 
   get '/users/new' do
+  	@user = User.new
   	erb :'users/new'
   end
 
   post '/users' do
-  	user = User.create(email: params[:email],
-  										password: params[:password])
-  	session[:user_id] = user.id
-  	redirect to('/links')
+  	@user = User.new(email: params[:email],
+  									password: params[:password],
+  									password_confirmation: params[:password_confirmation])
+  	if @user.save
+  		session[:user_id] = @user.id
+  		redirect to('/links')
+  	else
+  		flash.now[:errors] = @user.errors.full_messages
+  		erb :'users/new'
+  	end
   end
+
+  get '/sessions/new' do
+  	erb :'sessions/new'
+  end
+
+  post '/sessions' do
+  	user = User.authenticate(params[:email], params[:password])
+  	if user
+  		session[:user_id] = user.id
+  		redirect to('/links')
+  	else
+  		flash.now[:errors] = ['The email or password is incorrect']
+  		erb :'sessions/new'
+  	end
+  end
+
 
   helpers do
   	def current_user
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -4,4 +4,38 @@ feature 'user signup' do
 		expect(page).to have_content('Welcome, alice@example.com')
 		expect(User.first.email).to eq('alice@example.com')
 	end
+
+	scenario 'requires matching confirmation password' do
+		expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+		expect(current_path).to eq('/users')
+		expect(page).to have_content 'Password does not match the confirmation'
+	end
+
+	scenario "I can't sign up without an email address" do
+		expect { sign_up(email: nil) }.not_to change(User, :count)
+	end
+
+	scenario "I can't sign up with an invalid email address" do
+		expect { sign_up(email: 'invalid@email') }.not_to change(User, :count)
+	end
+
+	scenario 'I cannot sign up with an existing email address' do
+		sign_up
+		expect { sign_up }.not_to change(User, :count)
+		expect(page).to have_content('Email is already taken')
+	end
 end
+
+feature 'user sign in' do
+	let!(:user) do
+		User.create(email: 'user@example.com',
+								password: 'secret1234',
+								password_confirmation: 'secret1234')
+	end
+
+	scenario 'with correct credentials' do
+		sign_in(email: user.email, password: user.password)
+		expect(page).to have_content "Welcome, #{user.email}"
+	end
+end
+
